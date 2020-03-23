b8a348f12566ac2a18b25492d58e8123086caf23
dmitrii92 <fogot92@gmail.com>
2020-03-23 13:42:14 +0300

    Updates

---
+++ precache-manifest.a53b10398a3ba3f3f2cdb791e9618541.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81df9ff5a531303098d126004636dc92",
+    "url": "/formik-ts-starter/index.html"
+  },
+  {
+    "revision": "417dd7e44250db0315f9",
+    "url": "/formik-ts-starter/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f195785af7e9c56ae01d",
+    "url": "/formik-ts-starter/static/js/2.37bdce4c.chunk.js"
+  },
+  {
+    "revision": "92993e1feaccc30e8e07764395cfe931",
+    "url": "/formik-ts-starter/static/js/2.37bdce4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "417dd7e44250db0315f9",
+    "url": "/formik-ts-starter/static/js/main.e1372840.chunk.js"
+  },
+  {
+    "revision": "c1559eac4df22a22728c",
+    "url": "/formik-ts-starter/static/js/runtime-main.84407549.js"
+  }
+]);
